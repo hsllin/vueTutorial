1724b1cf70ea1dd70a45ece32bbb910b8733af9f
hsllin <1051701859@qq.com>
2018-12-17 19:36:02 +0800

    vue todo demo

---
+++ vueRouterDemo.vue
@@ -0,0 +1,10 @@
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
+<script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
+
+<div id="app">
+    <p>
+        <router-link to="/foo">GO to Foo</router-link>
+        <router-link to="/bar">Go to Bar</router-link>
+    </p>
+    <router-view></router-view>
+</div>
